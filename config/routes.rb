9d306a6510d78476d2b93d518c6152ffa46b1ad8
Bui Hung <bloodarchduke@gmail.com>
2017-12-12 00:59:13 +0700

    sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-get '/index', to: 'pages#index'
-get '/help', to: 'pages#help'
-get '/contact', to: 'pages#contact'
-get '/about', to: 'pages#about'
-root 'pages#index'
+  get 'users/new'
+
+get "/index", to: "pages#index"
+get "/help", to: "pages#help"
+get "/contact", to: "pages#contact"
+get "/about", to: "pages#about"
+get  '/signup',  to: 'users#new'
+post '/signup',  to: 'users#create'
+root "pages#index"
+resources :users
 end
